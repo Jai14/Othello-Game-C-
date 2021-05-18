b716061017d04ca8cf373144afa6e37abdb1b5f0
Jai14 <jai.sridharanmurali@ucdconnect.ie>
2021-05-19 01:15:23 +0530

    Create cmake_clean.cmake

---
+++ untitled5/cmake-build-debug/CMakeFiles/untitled5.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/untitled5.dir/check.c.obj"
+  "CMakeFiles/untitled5.dir/main.c.obj"
+  "CMakeFiles/untitled5.dir/make_moves.c.obj"
+  "CMakeFiles/untitled5.dir/set_board.c.obj"
+  "CMakeFiles/untitled5.dir/winner.c.obj"
+  "libuntitled5.dll.a"
+  "untitled5.exe"
+  "untitled5.exe.manifest"
+  "untitled5.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/untitled5.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
