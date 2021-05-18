09aaf7f750317e7a649c105ca6761edbd0453788
Jai14 <jai.sridharanmurali@ucdconnect.ie>
2021-05-19 01:12:48 +0530

    Create set_board.h

---
+++ untitled5/set_board.h
@@ -0,0 +1,10 @@
+//
+// Created by Jai Narayana on 18-05-2021.
+//
+#ifndef UNTITLED5_SET_BOARD_H
+#define UNTITLED5_SET_BOARD_H
+void getname();
+void initial();
+void countWB();
+void printboard();
+#endif //UNTITLED5_SET_BOARD_H
