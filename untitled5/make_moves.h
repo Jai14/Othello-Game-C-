49bb87c3289447cee0571ef71d53b90f724e7972
Jai14 <jai.sridharanmurali@ucdconnect.ie>
2021-05-19 01:12:42 +0530

    Create make_moves.h

---
+++ untitled5/make_moves.h
@@ -0,0 +1,8 @@
+//
+// Created by Jai Narayana on 18-05-2021.
+//
+#ifndef UNTITLED5_MAKE_MOVES_H
+#define UNTITLED5_MAKE_MOVES_H
+void Bmakemove();
+void Wmakemove();
+#endif //UNTITLED5_MAKE_MOVES_H
