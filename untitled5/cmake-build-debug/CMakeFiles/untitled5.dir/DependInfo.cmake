e99b4c1b3806e69548cdfaf2728358491f800572
Jai14 <jai.sridharanmurali@ucdconnect.ie>
2021-05-19 01:15:29 +0530

    Create DependInfo.cmake

---
+++ untitled5/cmake-build-debug/CMakeFiles/untitled5.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "C:/Users/Jai Narayana/CLionProjects/untitled5/check.c" "C:/Users/Jai Narayana/CLionProjects/untitled5/cmake-build-debug/CMakeFiles/untitled5.dir/check.c.obj"
+  "C:/Users/Jai Narayana/CLionProjects/untitled5/main.c" "C:/Users/Jai Narayana/CLionProjects/untitled5/cmake-build-debug/CMakeFiles/untitled5.dir/main.c.obj"
+  "C:/Users/Jai Narayana/CLionProjects/untitled5/make_moves.c" "C:/Users/Jai Narayana/CLionProjects/untitled5/cmake-build-debug/CMakeFiles/untitled5.dir/make_moves.c.obj"
+  "C:/Users/Jai Narayana/CLionProjects/untitled5/set_board.c" "C:/Users/Jai Narayana/CLionProjects/untitled5/cmake-build-debug/CMakeFiles/untitled5.dir/set_board.c.obj"
+  "C:/Users/Jai Narayana/CLionProjects/untitled5/winner.c" "C:/Users/Jai Narayana/CLionProjects/untitled5/cmake-build-debug/CMakeFiles/untitled5.dir/winner.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
