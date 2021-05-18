b61b9bc4e76f69bf73b94f7f626fe069a261dc02
Jai14 <jai.sridharanmurali@ucdconnect.ie>
2021-05-19 01:12:26 +0530

    Create main.c

---
+++ untitled5/main.c
@@ -0,0 +1,18 @@
+//Otello game
+//Software assignment-2
+//Done by Jai narayana
+//Student Number : 20204649
+
+//libraries
+#include "set_board.h"
+#include "make_moves.h"
+
+//main function
+int main()
+{
+    getname();  //takes the user names (player 1 and player 2)
+    initial();  //initializes the starting board
+    Bmakemove(); //BLACK moves first
+
+   return 0;
+}
